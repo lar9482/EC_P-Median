1e3bb9702fa8e25e68a4822a1e5fa755cd9a1664
Luke Runnels <lar9482@utulsa.edu>
2023-03-11 16:16:33 -0600

    Beginnning implementation for uniform crossover.
    Wrote feasibility helper function for crossover as sanity check.

--- crossover/crossover_methods.py
+++ crossover/crossover_methods.py
@@ -11,7 +11,13 @@
 import random
 import numpy as np
 
+def crossover_feasibility(parent1, parent2):
+    if (len(parent1) != len(parent2)):
+        raise Exception('ERROR: Two inputted parents cannot be crossed over')
+    
 def single_point(parent1, parent2):
+    crossover_feasibility(parent1, parent2)
+
     splice_point = int(random.uniform(1, len(parent1)-1))
 
     child1 = np.concatenate((parent1[0:splice_point:1], parent2[splice_point:len(parent2):1]))
@@ -20,6 +26,8 @@ def single_point(parent1, parent2):
     return (child1, child2)
 
 def double_point(parent1, parent2):
+    crossover_feasibility(parent1, parent2)
+
     first_splice_point = int(random.uniform(0, len(parent1)-2))
     second_splice_point = int(random.uniform(first_splice_point, len(parent1)-1))
 
@@ -32,4 +40,14 @@ def double_point(parent1, parent2):
     return(child1, child2)
 
 def uniform(parent1, parent2):
+    crossover_feasibility(parent1, parent2)
+    
+    uniform_bitstring = np.empty((len(parent1)), dtype=np.int32)
+
+    for i in range(0, len(uniform_bitstring)):
+        if (random.uniform(0, 1) < 0.5):
+            uniform_bitstring[i] = 0
+        else:
+            uniform_bitstring[i] = 1
+
     pass
